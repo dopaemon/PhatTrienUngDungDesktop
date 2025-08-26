f0faefe9dc214ad5cfa48d97aca0fff7a56786a8
dopaemon <polarisdp@gmail.com>
2025-08-26 05:45:03 -0700

    Winform: Add new GroupBox, rdDo (is Default), rdXanh, rdTrang
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BaiTapThietKeForm/frmBai1.Designer.cs
+++ BaiTapThietKeForm/frmBai1.Designer.cs
@@ -29,7 +29,12 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdDo = new System.Windows.Forms.RadioButton();
+            this.rdXanh = new System.Windows.Forms.RadioButton();
+            this.rdTrang = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -41,15 +46,64 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdTrang);
+            this.groupBox1.Controls.Add(this.rdXanh);
+            this.groupBox1.Controls.Add(this.rdDo);
+            this.groupBox1.Location = new System.Drawing.Point(480, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 258);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn màu xe";
+            // 
+            // rdDo
+            // 
+            this.rdDo.AutoSize = true;
+            this.rdDo.Checked = true;
+            this.rdDo.Location = new System.Drawing.Point(46, 48);
+            this.rdDo.Name = "rdDo";
+            this.rdDo.Size = new System.Drawing.Size(39, 17);
+            this.rdDo.TabIndex = 0;
+            this.rdDo.TabStop = true;
+            this.rdDo.Text = "Đỏ";
+            this.rdDo.UseVisualStyleBackColor = true;
+            // 
+            // rdXanh
+            // 
+            this.rdXanh.AutoSize = true;
+            this.rdXanh.Location = new System.Drawing.Point(46, 96);
+            this.rdXanh.Name = "rdXanh";
+            this.rdXanh.Size = new System.Drawing.Size(50, 17);
+            this.rdXanh.TabIndex = 1;
+            this.rdXanh.TabStop = true;
+            this.rdXanh.Text = "Xanh";
+            this.rdXanh.UseVisualStyleBackColor = true;
+            // 
+            // rdTrang
+            // 
+            this.rdTrang.AutoSize = true;
+            this.rdTrang.Location = new System.Drawing.Point(46, 144);
+            this.rdTrang.Name = "rdTrang";
+            this.rdTrang.Size = new System.Drawing.Size(53, 17);
+            this.rdTrang.TabIndex = 2;
+            this.rdTrang.TabStop = true;
+            this.rdTrang.Text = "Trắng";
+            this.rdTrang.UseVisualStyleBackColor = true;
+            // 
             // frmBai1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "frmBai1";
             this.Text = "frmBai1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -57,5 +111,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdTrang;
+        private System.Windows.Forms.RadioButton rdXanh;
+        private System.Windows.Forms.RadioButton rdDo;
     }
 }
