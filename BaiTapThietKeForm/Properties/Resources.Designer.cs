72b33562deb8b40621c4d3fe42323d0923fe602a
dopaemon <polarisdp@gmail.com>
2025-08-26 09:25:32 -0700

    Winform: Add initial frmBai4 and Logic for frmBai4_Load
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BaiTapThietKeForm/Properties/Resources.Designer.cs
+++ BaiTapThietKeForm/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace BaiTapThietKeForm.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap note4_1 {
+            get {
+                object obj = ResourceManager.GetObject("note4.1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
