5b493be72c61a283d7e25ff7abfe431eaf2f0e26
dopaemon <polarisdp@gmail.com>
2025-08-26 06:20:23 -0700

    Winform: Complete FrontEnd
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BaiTapThietKeForm/frmBai2.Designer.cs
+++ BaiTapThietKeForm/frmBai2.Designer.cs
@@ -28,12 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnChonHang = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnBoHang = new System.Windows.Forms.Button();
+            this.btnTinhTien = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSoTien = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnChonHang);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(348, 221);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblSoTien);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.btnTinhTien);
+            this.panel2.Controls.Add(this.btnBoHang);
+            this.panel2.Controls.Add(this.listBox2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(355, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(348, 221);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh sách hàng hoá";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Chuột",
+            "Bàn phím",
+            "Máy in",
+            "USB Kingmax"});
+            this.listBox1.Location = new System.Drawing.Point(34, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnChonHang
+            // 
+            this.btnChonHang.Location = new System.Drawing.Point(177, 87);
+            this.btnChonHang.Name = "btnChonHang";
+            this.btnChonHang.Size = new System.Drawing.Size(147, 23);
+            this.btnChonHang.TabIndex = 2;
+            this.btnChonHang.Text = "Chọn hàng >";
+            this.btnChonHang.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Danh sách hàng hoá";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(201, 51);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 3;
+            // 
+            // btnBoHang
+            // 
+            this.btnBoHang.Location = new System.Drawing.Point(25, 87);
+            this.btnBoHang.Name = "btnBoHang";
+            this.btnBoHang.Size = new System.Drawing.Size(147, 23);
+            this.btnBoHang.TabIndex = 3;
+            this.btnBoHang.Text = "< Bỏ hàng";
+            this.btnBoHang.UseVisualStyleBackColor = true;
+            // 
+            // btnTinhTien
+            // 
+            this.btnTinhTien.Location = new System.Drawing.Point(58, 123);
+            this.btnTinhTien.Name = "btnTinhTien";
+            this.btnTinhTien.Size = new System.Drawing.Size(75, 23);
+            this.btnTinhTien.TabIndex = 4;
+            this.btnTinhTien.Text = "Tính Tiền";
+            this.btnTinhTien.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tổng tiền thanh toán:";
+            // 
+            // lblSoTien
+            // 
+            this.lblSoTien.AutoSize = true;
+            this.lblSoTien.BackColor = System.Drawing.SystemColors.Control;
+            this.lblSoTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoTien.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblSoTien.Location = new System.Drawing.Point(172, 180);
+            this.lblSoTien.Name = "lblSoTien";
+            this.lblSoTien.Size = new System.Drawing.Size(52, 17);
+            this.lblSoTien.TabIndex = 6;
+            this.lblSoTien.Text = "0 đồng";
+            // 
+            // frmBai2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmBai2";
+            this.ClientSize = new System.Drawing.Size(705, 224);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmBai2";
+            this.Text = "Bán hàng";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnChonHang;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBoHang;
+        private System.Windows.Forms.Label lblSoTien;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTinhTien;
     }
 }
