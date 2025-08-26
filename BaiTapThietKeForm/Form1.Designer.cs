67e29627edc749bfd8bdf011a844b24d40122a77
dopaemon <polarisdp@gmail.com>
2025-08-26 05:27:46 -0700

    Winform: Add Initial MenuStrip with Bai1, Bai2, Bai3
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BaiTapThietKeForm/Form1.Designer.cs
+++ BaiTapThietKeForm/Form1.Designer.cs
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.trầnNguyễnTuấnAnhToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBai3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBai1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBai2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trầnNguyễnTuấnAnhToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // trầnNguyễnTuấnAnhToolStripMenuItem
+            // 
+            this.trầnNguyễnTuấnAnhToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiBai1,
+            this.tsmiBai2,
+            this.tsmiBai3});
+            this.trầnNguyễnTuấnAnhToolStripMenuItem.Name = "trầnNguyễnTuấnAnhToolStripMenuItem";
+            this.trầnNguyễnTuấnAnhToolStripMenuItem.Size = new System.Drawing.Size(142, 20);
+            this.trầnNguyễnTuấnAnhToolStripMenuItem.Text = "Trần Nguyễn Tuấn Anh";
+            // 
+            // tsmiBai3
+            // 
+            this.tsmiBai3.Image = global::BaiTapThietKeForm.Properties.Resources.note3;
+            this.tsmiBai3.Name = "tsmiBai3";
+            this.tsmiBai3.Size = new System.Drawing.Size(180, 22);
+            this.tsmiBai3.Text = "Bài 3";
+            // 
+            // tsmiBai1
+            // 
+            this.tsmiBai1.Image = global::BaiTapThietKeForm.Properties.Resources.note1;
+            this.tsmiBai1.Name = "tsmiBai1";
+            this.tsmiBai1.Size = new System.Drawing.Size(180, 22);
+            this.tsmiBai1.Text = "Bài 1";
+            // 
+            // tsmiBai2
+            // 
+            this.tsmiBai2.Image = global::BaiTapThietKeForm.Properties.Resources.note2;
+            this.tsmiBai2.Name = "tsmiBai2";
+            this.tsmiBai2.Size = new System.Drawing.Size(180, 22);
+            this.tsmiBai2.Text = "Bài 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem trầnNguyễnTuấnAnhToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBai1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBai2;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBai3;
     }
 }
 
