cd7e2458963d97444213da872e9e6c1248ef09a7
dopaemon <polarisdp@gmail.com>
2025-08-26 05:37:35 -0700

    Winform: Add new PictureBox
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BaiTapThietKeForm/Properties/Resources.Designer.cs
+++ BaiTapThietKeForm/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace BaiTapThietKeForm.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap car1 {
+            get {
+                object obj = ResourceManager.GetObject("car1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap note1 {
             get {
                 object obj = ResourceManager.GetObject("note1", resourceCulture);
