f15e07cfac1515f336de6ccc0fb7e5e7ede95d7f
dopaemon <polarisdp@gmail.com>
2025-08-26 05:42:39 -0700

    Winform: Rewrite frmBai1 (Other error)
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BaiTapThietKeForm/frmBai1.Designer.cs
+++ BaiTapThietKeForm/frmBai1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BaiTapThietKeForm.Properties.Resources.car1;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(365, 324);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmBai1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmBai1";
             this.Text = "frmBai1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
