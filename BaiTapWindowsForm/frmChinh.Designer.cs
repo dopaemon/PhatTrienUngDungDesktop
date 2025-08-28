b35e27b4082788061103d6b18c17faca1b5a5a1d
dopaemon <polarisdp@gmail.com>
2025-08-27 23:27:27 -0700

    [DO-NOT-MERGE] Winform: Add initial Form (maybe has some derp typo)
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BaiTapWindowsForm/frmChinh.Designer.cs
+++ BaiTapWindowsForm/frmChinh.Designer.cs
@@ -28,20 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbBai1 = new System.Windows.Forms.ToolStripButton();
+            this.tsbBai2 = new System.Windows.Forms.ToolStripButton();
+            this.tsbBai3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbBai1,
+            this.tsbBai2,
+            this.tsbBai3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(692, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbBai1
+            // 
+            this.tsbBai1.Image = global::BaiTapWindowsForm.Properties.Resources.note2;
+            this.tsbBai1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBai1.Name = "tsbBai1";
+            this.tsbBai1.Size = new System.Drawing.Size(52, 22);
+            this.tsbBai1.Text = "Bài 1";
+            // 
+            // tsbBai2
+            // 
+            this.tsbBai2.Image = global::BaiTapWindowsForm.Properties.Resources.note1;
+            this.tsbBai2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBai2.Name = "tsbBai2";
+            this.tsbBai2.Size = new System.Drawing.Size(52, 22);
+            this.tsbBai2.Text = "Bài 1";
+            this.tsbBai2.ToolTipText = "Bài 2";
+            // 
+            // tsbBai3
+            // 
+            this.tsbBai3.Image = global::BaiTapWindowsForm.Properties.Resources.note3;
+            this.tsbBai3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBai3.Name = "tsbBai3";
+            this.tsbBai3.Size = new System.Drawing.Size(52, 22);
+            this.tsbBai3.Text = "Bài 3";
             // 
             // frmChinh
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(692, 382);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "frmChinh";
             this.Text = "Chương trình chính";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbBai1;
+        private System.Windows.Forms.ToolStripButton tsbBai2;
+        private System.Windows.Forms.ToolStripButton tsbBai3;
     }
 }
 
